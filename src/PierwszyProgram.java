0f4211a2365d4ba2a71923ca20ed23a4a68e29e0
Daniel Szlicht <daniel.szlicht25@gmail.com>
2021-03-09 18:43:12 +0100

    git init

--- src/PierwszyProgram.java
+++ src/PierwszyProgram.java
@@ -1,8 +1,13 @@
 class PierwszyProgram {
 
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("\tHello" );
         System.out.println();
-        System.out.println("Witamy na kursie Javy!");
+        System.out.print("\tJeszcze jedna instrukcja");
+        System.out.println("...");
+        System.out.println("Wi\ntamy na kursie \"Java od Podstaw\" !");
+
+        System.out.println("Hello\n\nWitamy na kursie!");
+
     }
 }
