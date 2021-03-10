a9ceaf4e912eb5b2f3bc9cba5f9465ac90e77877
Daniel Szlicht <daniel.szlicht25@gmail.com>
2021-03-10 20:03:53 +0100

    ify

---
+++ src/instrukcjeWarunkowe/InstrukcjeWarunkoweDemo.java
@@ -0,0 +1,23 @@
+package instrukcjeWarunkowe;
+
+import java.util.Random;
+import java.util.Scanner;
+
+class InstrukcjeWarunkoweDemo {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Ile ścianek ma mieć kostka?");
+        int iloscScianek = scanner.nextInt();
+
+        if(iloscScianek < 2 ){
+            System.out.println("Nie można podać liczby mniejszej niż 2!");
+            System.exit(1); // zamyka aplikacje
+        }
+
+        Random random = new Random();
+        int wynikRzutu = random.nextInt(iloscScianek) +1;
+        System.out.println("Wylosowano " + wynikRzutu);
+
+    }
+}
