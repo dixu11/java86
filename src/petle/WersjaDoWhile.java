bd7309c74adf53dd975957df3968073a1b18c113
Daniel Szlicht <daniel.szlicht25@gmail.com>
2021-03-11 19:26:23 +0100

    do-while

--- src/petle/WersjaDoWhile.java
+++ src/petle/WersjaDoWhile.java
@@ -7,16 +7,16 @@ class WersjaDoWhile {
     public static void main(String[] args) {
 
 
-        Random random = new Random(); // bedziemy losować
+        Random random = new Random();
         int wylosowana;
-        int counter = 0; //liczy wykonania
-        int suma = 0; // do tego sumujemy liczby
-        do { // powtarzamy jeśli nie wylosowaliśmy 10
+        int counter = 0;
+        int suma = 0;
+        do {  //wykonaj te rzeczy (wykonasie minumum raz)
             wylosowana = random.nextInt(20);
-            suma += wylosowana; // "dorzucam" teraz wylosowaną
+            suma += wylosowana;
             System.out.println(wylosowana);
             counter++;
-        }while (  wylosowana != 10  );
+        }while (  wylosowana != 10  ); // powtorz jesli spelnimy warunek
         System.out.println("Znaleziono po " + counter + " rzutów");
         System.out.println("Suma:  " + suma);
 
