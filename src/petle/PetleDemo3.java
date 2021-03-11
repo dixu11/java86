4ff000e4ce265e630b3d0e4c751d60bf31af66c5
Daniel Szlicht <daniel.szlicht25@gmail.com>
2021-03-11 19:00:14 +0100

    zad 10 i 13

---
+++ src/petle/PetleDemo3.java
@@ -0,0 +1,13 @@
+package petle;
+
+class PetleDemo3 {
+    public static void main(String[] args) {
+        int liczba = 20;
+        while (liczba<=30){
+            if (liczba != 25) {
+                System.out.println(liczba);
+            }
+            liczba++;
+        }
+    }
+}
