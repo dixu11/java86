345ef404d2a743cb8f227a2760f6aee1ad67e5c2
Daniel Szlicht <daniel.szlicht25@gmail.com>
2021-03-29 20:08:23 +0200

    obiektowość wstęp

--- src/obiektowe/wstep/ObiektoweWstep.java
+++ src/obiektowe/wstep/ObiektoweWstep.java
@@ -10,17 +10,35 @@ public class ObiektoweWstep {
         String zdanie = new String("Ala ma kota");
         int[] liczby = new int[]{23, 56, 67, 23};
 
-       // MojaKlasa mojObiekt1 = new MojaKlasa();
+      // MojaKlasa mojObiekt1 = new MojaKlasa();
         Osoba osoba1 = new Osoba();
         Osoba osoba2 = new Osoba();
 
 //        Osoba.imie  // tak dostałbym się tylko do statycznej składowej
 
-        osoba1.imie = "Radek";
-        osoba2.imie = "Marcin";
+        osoba1.nazwa = "Radek";
+        osoba2.nazwa = "Marcin";
 
-        System.out.println(osoba1.imie);
-        System.out.println(osoba2.imie);
+        System.out.println(osoba1.nazwa);
+        System.out.println(osoba2.nazwa);
+
+        osoba1.wiek = 30;
+
+        System.out.println(osoba1.wiek);
+        System.out.println(osoba2.wiek);
+
+       // Osoba.wyswietlDane();
+
+        osoba1.wyswietlDane();
+        osoba2.wyswietlDane();
+
+        osoba1.wyprawUrodziny();
+        osoba1.wyswietlDane();
+
+        osoba2.dodajNazwisko("Kowalski");
+        if (osoba2.czyPelnoletni()) {
+            System.out.println(osoba2.nazwa + " jest pełnoletni");
+        }
 
     }
 }
--- src/obiektowe/wstep/Osoba.java
+++ src/obiektowe/wstep/Osoba.java
@@ -3,8 +3,33 @@ package obiektowe.wstep;
 public class Osoba {
 
     // cechy - pola
-    String imie;
+    String nazwa;
+    int wiek = 18;
 
     // zachowania - metody
+     void wyswietlDane(){
+        System.out.println("Mam na imie " + nazwa);
+        System.out.println("Mam lat: " + wiek);
+    }
+
+    void wyprawUrodziny() {
+         wiek+=1;
+        System.out.println(nazwa + " obchodzi urodziny!");
+    }
+
+    boolean czyPelnoletni() {
+        if (wiek >= 18) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    void dodajNazwisko(String nazwisko) {
+        nazwa += " " + nazwisko;
+    }
+
+
+
 
 }
