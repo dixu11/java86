3dc6f7442081b7d9a32ed865b9913ef758f326ca
Daniel Szlicht <daniel.szlicht25@gmail.com>
2021-04-13 20:14:05 +0200

    metody

--- src/powtorki/Metody.java
+++ src/powtorki/Metody.java
@@ -6,26 +6,34 @@ class Metody {
 
 
     public static void main(String[] args) {
+        //args
         sayHelloTo("Zofia");
         sayHelloTo("Ada");
-        sayHelloTo("Marian","Adam",3);
-
+        sayHelloTo("Marian", "Adam", 3);
         greet();
 
         Random random = new Random();
+
+        //return values
         int generatedNumber = random.nextInt(5);
         System.out.println(generatedNumber);
 
         int lucky = getLuckyNumber() + 3;
         System.out.println(lucky);
 
-        System.out.println(add(2,2));
+        System.out.println(add(2, 2));
 
         int result = countPower(3);
         System.out.println(result);
 
-
+        //recursion
         System.out.println(countPowerRecursion(3));
+
+        //varargs
+        showNumbers(1);
+        showNumbers(1, 45, 67);
+        showNumbers();
+        showNumbers(456, 56, 676, 76, 787, 878, 67, 5645, 343, 2, 323, 12, 12, 34, 4, 5, 67, 8);
     }
 
     //nagłówek / sygnatura
@@ -36,12 +44,12 @@ class Metody {
 
     //  |modyfikator dostępu| obiektowe/nieobiektowe | typ zwracany | nazwa | ( parametry )
 
-    public static void greet ( )  {
+    public static void greet() {
         System.out.println("Hello");
 
     }
 
-    public static void sayHelloTo( String name){
+    public static void sayHelloTo(String name) {
         System.out.println("Hi " + name);
     }
 
@@ -73,7 +81,15 @@ class Metody {
         if (power == 0) {
             return 1;
         }
-       return countPowerRecursion(power -1)*2;
+        return countPowerRecursion(power - 1) * 2;
+    }
+
+
+    public static void showNumbers(int... numbers) { // varargs - dowolna ilość argumentów
+        for (int number : numbers) {
+            System.out.print(number + ", ");
+        }
+        System.out.println();
     }
 
 }
